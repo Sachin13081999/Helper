acf4498a1453d893559d58d6e436aa1b2f2fd4ec
Sachin Thenua <sachinthenua13081999@gmail.com>
2022-09-20 23:30:34 +0530

    commit

--- components/layouts/Form/Components/FormLeftWrapper.js
+++ components/layouts/Form/Components/FormLeftWrapper.js
@@ -4,9 +4,13 @@ const FormLeftWrapper = () => {
     <FormLeft>
       <FormInput>
       <label>Campaign Title</label>
-      <Input>
-      
+      <Input placeholder= 'Campaign Title'>
       </Input>
+      </FormInput>
+      <FormInput>
+      <label>Story</label>
+      <TextArea placeholder= 'Describe Your Story'> 
+      </TextArea>
       </FormInput>
     </FormLeft>
   )
@@ -20,12 +24,31 @@ const FormInput = styled.div`
 display:flex;
 flex-direction:column;
 font-family:'poppins';
-
+margin-top:10px;
 `
 
 const Input = styled.input`
-padding:8px;
+padding:15px;
 background-color:${(props) => props.theme.bgDiv};
-
+color:${(props) => props.theme.color};
+margin-top:4px;
+border:none;
+border-radius:8px;
+outline:none;
+font-size:large;
+width:100%;
+overflow-x:hidden;
+`
+const TextArea = styled.textarea`
+padding:15px;
+background-color:${(props) => props.theme.bgDiv};
+color:${(props) => props.theme.color};
+margin-top:4px;
+border:none;
+border-radius:8px;
+outline:none;
+font-size:large;
+max-width:100%;
+min-width:100%;
 `
 export default FormLeftWrapper
--- components/layouts/Form/Components/FormRightWrapper.js
+++ components/layouts/Form/Components/FormRightWrapper.js
@@ -1,7 +1,76 @@
+import styled from 'styled-components';
+
 const FormRightWrapper = () => {
   return (
-    <div>FormRightWrapper</div>
+    <FormRight>
+     <FormInput>
+       <FormRow>
+         <RowFirstInput>
+            <label>Required Amount</label>
+            <Input type={'number'} placeholder='Required Amount'></Input>
+         </RowFirstInput>
+         <RowSecondInput>
+            <label>Choose Category</label>
+            <Select>
+              <option>Education</option>
+              <option>Health</option>
+              <option>Other</option>
+            </Select>
+         </RowSecondInput>
+        </FormRow>
+     </FormInput>
+    </FormRight>
   )
 }
+
+const FormRight = styled.div`
+width:45%;
+`
+
+const FormInput = styled.div`
+display:flex;
+flex-direction:column;
+font-family:'poppins';
+margin-top:10px;
+`
+
+const FormRow = styled.div`
+  display: flex;
+  justify-content:space-between;
+  width:100% ;
+`
+const Input = styled.input`
+padding:15px;
+background-color:${(props) => props.theme.bgDiv};
+color:${(props) => props.theme.color};
+margin-top:4px;
+border:none;
+border-radius:8px;
+outline:none;
+font-size:large;
+width:100%;
+`
+const RowFirstInput = styled.div`
+  display:flex ;
+  flex-direction:column ;
+  width:45% ;
+`
+
+const RowSecondInput = styled.div`
+  display:flex ;
+  flex-direction:column ;
+  width:45% ;
+`
+const Select = styled.select`
+  padding:15px;
+  background-color:${(props) => props.theme.bgDiv} ;
+  color:${(props) => props.theme.color} ;
+  margin-top:4px;
+  border:none ;
+  border-radius:8px ;
+  outline:none;
+  font-size:large;
+  width:100% ;
+  `
 
 export default FormRightWrapper
